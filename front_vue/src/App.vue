0909954a2e756f5bad4f6f399329cddf22cc2f30
libbrary25252 <n.laiying25@gmail.com>
2023-03-14 18:18:11 +0800

    build menu

--- front_vue/src/App.vue
+++ front_vue/src/App.vue
@@ -1,5 +1,61 @@
 <template>
-<div>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <router-link to="/" class="navbar-item"><strong>Smart Campus</strong></router-link>
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar_menu" @click="showMobileMenu = !showMobileMenu">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbar_menu" class="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
+    <div class="navbar-start">
+      <router-link to="/home" class="navbar-item">
+        Home
+      </router-link>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          CRS
+        </a>
+
+        <div class="navbar-dropdown">
+          <router-link to="/createQList" class="navbar-item">
+            Question List
+          </router-link>
+          <router-link to="/genQList" class="navbar-item">
+            Search Category
+          </router-link>
+
+          <hr class="navbar-divider">
+          <router-link to="/report" class="navbar-item">
+            Report an issue
+          </router-link>
+        </div>
+      </div>
+
+      <router-link to="/activity" class="navbar-item">
+        Activity
+      </router-link>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <router-link to="/profile" class="button is-primary">
+            <strong>Profile</strong>
+          </router-link>
+          <router-link to="/login" class="button is-light">
+            Log in
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+<!-- <div>
 <nav class="navbar is-dark">
   <div class="navbar-brand">
   <router-link to="/" class="navbar-item"><strong>Smart Campus</strong></router-link>
@@ -13,8 +69,8 @@
   <div class="navbar-menu" id="navbar_menu" v-bind:class="{'is-active': showMobileMenu}">
     <div class="navbar-start">
       <router-link to="/createQList" class="navbar-item">CRS</router-link>
-      <router-link to="/database" class="navbar-item">Database</router-link><!-- need filter the user type -->
-      <router-link to="/activity" class="navbar-item">Activity</router-link><!-- need integrate with milk box-->
+      <router-link to="/report" class="navbar-item">Report</router-link>  need filter the user type 
+      <router-link to="/activity" class="navbar-item">Activity</router-link>  need integrate with milk box
     </div>
     <div class="navbar-end">
       <div class="navbar-item">
@@ -28,18 +84,14 @@
       </div>
     </div>
   </div>
-</nav>
-
-
+</nav> -->
 
   <section class="section">
     <router-view/>
   </section>
-
   <footer class="footer">
     <p class="has-text-centered">Copyright (c) 2022</p>
   </footer>
-</div>
 </template>
 
 <script>
--- front_vue/src/main.js
+++ front_vue/src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import Vue from 'vue'
+// import VueSidebarMenu from 'vue-sidebar-menu'
+// import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 
 createApp(App).use(store).use(router).mount('#app')
