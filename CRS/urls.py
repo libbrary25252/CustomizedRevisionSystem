c32993ab6bcde6a4600190045a75f75f1c64b942
libbrary25252 <n.laiying25@gmail.com>
2023-03-17 00:00:03 +0800

    ContainerAPI

--- CRS/urls.py
+++ CRS/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path, re_path
 from . import views
-from .views import QuestionAPI
+from .views import QuestionAPI, ContainerAPI
 
 # URLConfiguration
 urlpatterns = [
     path('', views.home, name='Home'),
-    re_path('questions', QuestionAPI.as_view())
+    re_path('questions', QuestionAPI.as_view(),),
+    re_path('containers', ContainerAPI.as_view())
 ]
--- CRS/views.py
+++ CRS/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Question
+from .models import Question, QuestionQuestion
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import uuid
-from .serializers import QuestionSerializer
+from .serializers import QuestionSerializer, ContainerSerializer
 
 def home(request):
     return render(request, 'home.html', {'name': 'Hello'})
@@ -46,4 +46,38 @@ class QuestionAPI(APIView):
         new_question = Question.objects.create(QID=question_data["QID"], statement=question_data["statement"], string=question_data["string"], Qtype=question_data["Qtype"], description=question_data["description"], options=question_data["options"])
         new_question.save()
         serializer = QuestionSerializer(new_question)
+        return Response(serializer.data)
+    
+    class ContainerAPI(APIView):
+        serializer_class = ContainerSerializer
+
+    def get_queryset(self):
+        containers = QuestionQuestion.objects.all()
+        return containers
+
+    def get(self, request, *args, **kwargs):
+        try:
+            state_id = request.query_param["state_id"]
+            statement = request.query_param["statement"]
+            qid = request.query_param["qid"]
+            
+            if state_id != None:
+                state_id = QuestionQuestion.objects.get(state_id=state_id)
+                serializer = ContainerSerializer(state_id)
+            if statement != None:
+                statement = QuestionQuestion.objects.get(statement=statement)
+                serializer = ContainerSerializer(statement)
+            if qid != None:
+                qid = QuestionQuestion.objects.get(qid=qid)
+                serializer = ContainerSerializer(qid)
+        except:
+            containers = self.get_queryset()
+            serializer = ContainerSerializer(containers, many=True) #get all
+        return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+        container_data = request.data
+        new_container = Question.objects.create(state_id=container_data["state_id"], statement=container_data["statement"], qid=container_data["qid"])
+        new_container.save()
+        serializer = ContainerSerializer(new_container)
         return Response(serializer.data)
